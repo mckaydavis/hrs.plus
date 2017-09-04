49e54cac6638928bc0f99b1f1ace4354423525a0
rbvea <russellbvea@gmail.com>
2017-09-04 11:00:00 -1000

    Adding back server file
    [ref #1 #11]

---
+++ backend/app/src/server.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+const { EXPRESS_SERVER_PORT } = process.env;
+
+const server = app.listen(EXPRESS_SERVER_PORT, () => {
+  console.log(`> Listening on http://localhost:${server.address().port}`);
+});
