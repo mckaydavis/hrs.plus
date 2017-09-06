163d9eb14bc687963797db81f95e55d7b09816b1
Russell Vea <russellbvea@gmail.com>
2017-09-05 14:41:42 -1000

    Adding HMR, styled-components (server/client)
    [ref #1]

--- backend/app/src/app.jsx
+++ backend/app/src/app.jsx
@@ -10,19 +10,25 @@ const routesFile = path.resolve(__dirname, "./public/routes.jsx");
 const Routes = require(routesFile);
 
 const Base = require(path.resolve(__dirname, "./public/components/base.jsx"));
+const { ServerStyleSheet, StyleSheetManager } = require("styled-components");
 const { renderToString } = require("react-dom/server");
+
 app.set("view engine", "jsx");
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
+const sheet = new ServerStyleSheet();
+
 app.use("/", function(req, res) {
   res.send(
     renderToString(
-      <Base>
-        <StaticRouter location={req.url} context={{}}>
-          <Routes />
-        </StaticRouter>
-      </Base>
+      <StyleSheetManager sheet={sheet.instance}>
+        <Base>
+          <StaticRouter location={req.url} context={{}}>
+            <Routes />
+          </StaticRouter>
+        </Base>
+      </StyleSheetManager>
     )
   );
 });
--- backend/app/src/public/components/home.jsx
+++ backend/app/src/public/components/home.jsx
@@ -1,5 +1,18 @@
 const React = require("react");
+const styled = require("styled-components").default;
 
-const Home = (module.exports = () => {
-  return <h1>Home</h1>;
-});
+class Home extends React.Component {
+  render() {
+    const Test = styled.div`font-size: 12px;`;
+    return (
+      <div>
+        <Test>
+          <p>Hmm</p>
+        </Test>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Home;
